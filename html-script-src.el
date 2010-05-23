1e4188babe4f150a27cc4807c5afcee7a3b13cfd
Johan Andersson <johan.rejeep@gmail.com>
2010-05-23 21:48:43 +0200

    Added file comments.

--- html-script-src.el
+++ html-script-src.el
@@ -29,6 +29,18 @@
 
 ;;; Commentary:
 
+;; Inserts a script tag for HTML and HAML documents with a URL to a
+;; given JavaScript framework taken from: http://scriptsrc.net/
+
+;; To use this, make sure that this file is in Emacs load-path
+;; (add-to-list 'load-path "/path/to/directory/or/file")
+;;
+;; Then require it
+;; (require 'html-script-src)
+;;
+;; Then in your HTML or HAML file, run the function `html-script-src'.
+
+
 ;;; Code:
 
 
