6bd65505c682abc0466803435a8435763a814e97
Johan Andersson <johan.rejeep@gmail.com>
2010-05-23 20:59:26 +0200

    Added skeleton.

---
+++ html-script-src.el
@@ -0,0 +1,37 @@
+;;; html-script-src.el --- Insert <script src=".."> for popular JavaScript libraries
+
+;; Copyright (C) 2010 Johan Andersson
+
+;; Author: Johan Andersson <johan.rejeep@gmail.com>
+;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
+;; Version: 0.0.1
+;; Keywords: 
+;; URL: http://github.com/rejeep/html-script-src
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+
+(provide 'html-script-src)
+
+;;; html-script-src.el ends here
