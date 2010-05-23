c08d7cc25800e1b96be5eca2cb98f36b9129149a
Johan Andersson <johan.rejeep@gmail.com>
2010-05-23 21:41:41 +0200

    Added working code without comments.

--- html-script-src.el
+++ html-script-src.el
@@ -32,6 +32,66 @@
 ;;; Code:
 
 
+(defvar html-script-src-completion-fn
+  (if (fboundp 'ido-mode) 'ido-completing-read 'completing-read)
+  "")
+
+(defconst html-script-src-re
+  "<textarea id=\"fe_text_\\(.+\\)\".*class=\"fetext\".*>\\(.+\\)</textarea>"
+  "")
+
+(defconst html-script-src-scriptsrc-url "http://scriptsrc.net/"
+  "")
+
+(defconst html-script-src-haml-script-format
+  "%%script{ :src => '%s', :type => 'text/javascript', :charset => 'utf-8' }"
+  "")
+
+(defconst html-script-src-html-script-format
+  "<script src='%s' type='text/javascript' charset='utf-8'></script>"
+  "")
+
+(defun html-script-src ()
+  ""
+  (interactive)
+  (let* ((frameworks (html-script-src-frameworks))
+         (framework (funcall html-script-src-completion-fn "Framework: " (mapcar (lambda (x) (car x)) frameworks) nil t)))
+    (html-script-src-insert-tag (cdr (assoc framework frameworks)))))
+
+(defun html-script-src-frameworks ()
+  ""
+  (let ((buffer (html-script-src-fetch)))
+    (with-current-buffer buffer
+      (html-script-src-parse))))
+
+(defun html-script-src-parse ()
+  ""
+  (goto-char (point-min))
+  (let ((frameworks))
+    (while (re-search-forward "<textarea id=\"fe_text_\\(.+\\)\".*class=\"fetext\".*>\\(.+\\)</textarea>" nil t)
+      (add-to-list 'frameworks
+                   (cons
+                    (match-string-no-properties 1)
+                    (match-string-no-properties 2))))
+    frameworks))
+
+(defun html-script-src-fetch ()
+  ""
+  (let ((url-request-method "GET")
+        (url-request-extra-headers nil)
+        (url-mime-accept-string "*/*")
+        (url (url-generic-parse-url html-script-src-scriptsrc-url)))
+    (url-retrieve-synchronously url)))
+
+(defun html-script-src-insert-tag (url)
+  ""
+  (let ((format
+         (if (eq major-mode 'haml-mode)
+             (html-script-src-insert-haml-format)
+           (html-script-src-insert-html-format))))
+    (insert (format format url))))
+
+
 (provide 'html-script-src)
 
 ;;; html-script-src.el ends here
